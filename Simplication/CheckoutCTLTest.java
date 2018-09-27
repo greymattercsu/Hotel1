ec724e7f0fa8ea572c37176232be0d20918e8dc0
greymattercsu <41317773+greymattercsu@users.noreply.github.com>
2018-09-27 20:05:11 +1100

    Adding simplification folder
    Adding simplification folder, and adding basic Junit methods.

---
+++ Simplication/CheckoutCTLTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import hotel.HotelHelper;
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import hotel.entities.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+    
+
+/**
+ *
+ * @author PC
+ */
+public class CheckoutCTLTest {
+    
+    
+    public CheckoutCTLTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+
+
+    
+}

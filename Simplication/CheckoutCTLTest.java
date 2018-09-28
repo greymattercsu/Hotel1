e639e6f764aee7ba5263af86977adac471ffb571
greymattercsu <41317773+greymattercsu@users.noreply.github.com>
2018-09-28 20:11:18 +1100

    creating an hotel object
    Creating hotel object using the hotelhelper class.

--- Simplication/CheckoutCTLTest.java
+++ Simplication/CheckoutCTLTest.java
@@ -20,42 +20,58 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-
-    
-
 /**
  *
  * @author PC
  */
 public class CheckoutCTLTest {
 
-    //creating a byteArrayOutputStream object
-    //declaring and instanciating
+    // creating a byteArrayOutputStream object
+    // declaring and instanciating
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    
+
     public CheckoutCTLTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        //prints out the output of the method
+        // prints out the output of the method
         System.setOut(new PrintStream(outContent));
-//        System.setErr(new PrintStream(errContent));
-        
+        // System.setErr(new PrintStream(errContent));
+
     }
-    
+
     @After
     public void tearDown() {
     }
+
+    //
+    // /**
+    // * Test of roomIdEntered method, of class CheckoutCTL.
+    // */
+@Test
+public void testRoomIdEntered() {
+    System.out.println("roomIdEntered");
+    
+    //declaring the hotel object
+    Hotel hotel = null;
+    
+    //trying the try catch so that any exception that is occured will be captured.
+    try {
+        //instanciating the hotel object
+        hotel = HotelHelper.loadHotel();
+    } catch (Exception ex) {
+        Logger.getLogger(CheckoutCTLTest.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
 
 
 
